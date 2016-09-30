f809dcbbb3d4084739d627db3f9c90afa653ca7a
mrduan <yomingyo@gmail.com>
2016-09-30 12:06:57 +0800

    Create a project name is YueXian

---
+++ YueXian/YueXian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YueXian
+//
+//  Created by qingyun on 16/9/30.
+//  Copyright © 2016年 @Mr.Duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
